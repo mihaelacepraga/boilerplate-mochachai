2abc0501e041fe46fde2d11296bdc46a9f43e45b
mihaelacepraga <98689522+mihaelacepraga@users.noreply.github.com>
2023-02-12 18:27:38 +0000

    basic assertions #isOk  and #isNotOk

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -14,30 +14,30 @@ suite('Unit Tests', function() {
       assert.isUndefined(undefined, 'undefined IS undefined');
       assert.isDefined('hello', 'A string is not undefined');
     });
-    // // #3
-    // test('#isOk, #isNotOk', function () {
-    //   assert.fail(null, 'null is falsey');
-    //   assert.fail("I'm truthy", 'A string is truthy');
-    //   assert.fail(true, 'true is truthy');
-    // });
-    // // #4
-    // test('#isTrue, #isNotTrue', function () {
-    //   assert.fail(true, 'true is true');
-    //   assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-    //   assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
-    // });
+    // #3
+    test('#isOk, #isNotOk', function () {
+      assert.isNotOk(null, 'null is falsey');
+      assert.isOk("I'm truthy", 'A string is truthy');
+      assert.isOk(true, 'true is truthy');
+    });
+    // #4
+    test('#isTrue, #isNotTrue', function () {
+      assert.isTrue(true, 'true is true');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+    });
   });
 
   // -----------------------------------------------------------------------------
 
-  // suite('Equality', function () {
-  //   // #5
-  //   test('#equal, #notEqual', function () {
-  //     assert.fail(12, '12', 'Numbers are coerced into strings with ==');
-  //     assert.fail({ value: 1 }, { value: 1 }, '== compares object references');
-  //     assert.fail(6 * '2', '12');
-  //     assert.fail(6 + '2', '12');
-  //   });
+  suite('Equality', function () {
+    // #5
+    test('#equal, #notEqual', function () {
+      assert.fail(12, '12', 'Numbers are coerced into strings with ==');
+      assert.fail({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.fail(6 * '2', '12');
+      assert.fail(6 + '2', '12');
+    });
   //   // #6
   //   test('#strictEqual, #notStrictEqual', function () {
   //     assert.fail(6, '6');
